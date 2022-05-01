5cb4a5246dd538c3ba586d7ddf852033a3cbadd7
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2022-05-01 17:18:32 +0200

    Removed used files

--- chapter-7/2.vite-ssr/6.adding-layouts/2.using-component/src/main.js
+++ chapter-7/2.vite-ssr/6.adding-layouts/2.using-component/src/main.js
@@ -5,16 +5,12 @@ import './styles'
 
 // app.js (shared between server and client)
 import App from './App.vue'
-import store from './store'
 import { createSSRApp } from 'vue'
 import { createRouter } from './router'
 
 export function createApp() {
   const app = createSSRApp(App)
   const router = createRouter()
-  app
-    .use(router)
-    .use(store)
-
+  app.use(router)
   return { app, router }
 }
--- chapter-7/2.vite-ssr/6.adding-layouts/2.using-component/src/store/mutations.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-export default {
-  setLayout (state, data) {
-    state.layout = data
-  }
-}
